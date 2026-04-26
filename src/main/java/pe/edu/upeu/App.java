2f124f21f59b6b1b3d5e386c2f2740b8f72fc29d
ErickGonzalo <apazaerickcr7@gmail.com>
2026-04-25 21:23:14 -0500

    Proyecto CRUD de escuelas en JavaFX

--- src/main/java/pe/edu/upeu/App.java
+++ src/main/java/pe/edu/upeu/App.java
@@ -12,7 +12,6 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        // Cargar FXML
         URL fxml = getClass().getResource("/escuela.fxml");
 
         if (fxml == null) {
@@ -22,7 +21,6 @@ public class App extends Application {
         FXMLLoader loader = new FXMLLoader(fxml);
         Scene scene = new Scene(loader.load(), 600, 500);
 
-        // 👉 CARGAR CSS (BOTONES CON COLOR)
         scene.getStylesheets().add(
                 getClass().getResource("/styles.css").toExternalForm()
         );
